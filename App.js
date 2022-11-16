12ba6be4c3f673c1e5799b1b56bae8a3fbe4c4d7
Ayub <ayub.njambuya@opibus.se>
2022-11-16 10:18:56 +0300

    Feat: added API Fetch Method

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component() {
+  state ={
+    data: {'activity': 'loading...'} // json data recieved from the request
+  }
+  
+  
+  getJsonData = () => {
+    fetch("http://www.boredapi.com/api/activity?type=recreational/",
+    {method: 'GET'}).then((response) => response.json()).then((responseJson)=> {
+      console.log(responseJson);
+      this.setState({
+        data: responseJson
+      })
+    })  
+    .catch((error)=> {
+      console.error(error)
+    })
+  }
+
+  useEffect = (() =>{
+    this.getJsonData()
+  });
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 20, 
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
